b3e2e8a313c9a67f7c6e50dbe245f77fb4dd4592
AlvaroCoo <alvarocooi.sanjosemlg@fundacionloyola.net>
2024-10-21 08:52:55 +0200

    Mostrar mensaje de las respuestas en un modal

--- main.js
+++ main.js
@@ -112,9 +112,52 @@ let comprobarRespuestas = (respuesta) => {
             aciertos++;
     });
 
-    alert(aciertos + " aciertos de " + preguntas.length)
+    let modal = crearModal("Result", aciertos + " aciertos de " + preguntas.length);
 
+    modal.style.visibility = "visible"  
+    modal.style.opacity = 1
+}
 
+let crearModal = (tittle, text) => {
+
+    const modal = document.createElement("div")
+    modal.classList.add("modal")
+    
+    const modalContent = document.createElement("div")
+    modalContent.classList.add("modal-content")
+    
+    const closeModalBtn = document.createElement("span")
+    closeModalBtn.classList.add("modal-close")
+    closeModalBtn.innerHTML = '&times;'
+    modalContent.appendChild(closeModalBtn)
+    
+    closeModalBtn.addEventListener("click", function(){
+        modal.style.visibility = "hidden";
+        modal.style.opacity = "0";
+    })
+    
+    const modalTitle = document.createElement("h2")
+    modalTitle.textContent = tittle
+    modalContent.appendChild(modalTitle)
+
+    const modalHR = document.createElement("hr")
+    modalContent.appendChild(modalHR)
+    
+    const modalParagraph = document.createElement("p")
+    modalParagraph.textContent = text
+    modalContent.appendChild(modalParagraph)
+    
+    modal.appendChild(modalContent)
+    document.body.appendChild(modal)
+
+    window.addEventListener("click", function(event){
+        if (event.target === modal) {
+            modal.style.visibility = "hidden";
+            modal.style.opacity = "0";
+        }
+    })
+
+    return modal;
 }
 
 let numPregunta = 0
